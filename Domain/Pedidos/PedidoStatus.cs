6fe5fff693a15ab58ebf0a35ba67f1e618cd29a4
Christian <christiandmelo@gmail.com>
2023-08-24 15:11:50 -0300

    Atualizando o exemplo do swagger

--- Domain/Pedidos/PedidoStatus.cs
+++ Domain/Pedidos/PedidoStatus.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Domain.Pedidos
+﻿namespace Domain.Pedidos
 {
     public enum PedidoStatus
     {
