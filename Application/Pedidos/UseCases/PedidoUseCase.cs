5afd0da5a481281db0d362ac80b4cedcf5dab8f6
Christian <christiandmelo@gmail.com>
2023-08-25 14:34:56 -0300

    Ajuste no erro de adicao de pedidos

--- Application/Pedidos/UseCases/PedidoUseCase.cs
+++ Application/Pedidos/UseCases/PedidoUseCase.cs
@@ -2,11 +2,8 @@
 using Domain.Pedidos;
 using Domain.Base.DomainObjects;
 using Application.Pedidos.Events;
-using Application.Pedidos.Queries.DTO;
-using Domain.Base.Messages.CommonMessages.Notifications;
-using Domain.Base.Messages;
-using System.Runtime.CompilerServices;
 using Domain.Base.DomainObjects.DTO;
+using Application.Pedidos.Queries.DTO;
 using Domain.Base.Messages.CommonMessages.IntegrationEvents.Pedidos;
 
 namespace Application.Pedidos.UseCases
@@ -32,39 +29,48 @@ namespace Application.Pedidos.UseCases
         #region Use Cases
         public async Task<bool> AdicionarItem(Guid clienteId, Guid produtoId, string nome, int quantidade, decimal valorUnitario)
         {
+            var pedido = await _pedidoRepository.ObterPedidoRascunhoPorClienteId(clienteId);
             var pedidoItem = new PedidoItem(produtoId, nome, quantidade, valorUnitario);
-            var pedido = await BuscarOuIniciarPedidoAsync(clienteId, produtoId, pedidoItem);
 
-            var pedidoItemExistente = pedido.PedidoItemExistente(pedidoItem);
-            pedido.AdicionarItem(pedidoItem);
+            // Assim que cliente colocar um item no carrinho ele ja iniciou um pedido.
+            if (pedido is null)
+            {
+                pedido = Pedido.PedidoFactory.NovoPedidoRascunho(clienteId);
+                pedido.AdicionarItem(pedidoItem);
 
-            if (pedidoItemExistente)
-                _pedidoRepository.AtualizarItem(pedidoItem);
+                _pedidoRepository.Adicionar(pedido);
+                pedido.AdicionarEvento(new PedidoRascunhoIniciadoEvent(clienteId, produtoId));
+            }
             else
-                _pedidoRepository.AdicionarItem(pedidoItem);
+            {
+                var pedidoItemExistente = pedido.PedidoItemExistente(pedidoItem);
+                pedido.AdicionarItem(pedidoItem);
 
-            _pedidoRepository.Atualizar(pedido);
+                if (pedidoItemExistente)
+                {
+                    _pedidoRepository.AtualizarItem(pedido.PedidoItems.FirstOrDefault(p => p.ProdutoId == pedidoItem.ProdutoId));
+                }
+                else
+                {
+                    _pedidoRepository.AdicionarItem(pedidoItem);
+                }
+            }
 
             pedido.AdicionarEvento(new PedidoItemAdicionadoEvent(pedido.ClienteId, pedido.Id, produtoId, nome, valorUnitario, quantidade));
             return await _pedidoRepository.UnitOfWork.Commit();
         }
-
         public async Task<bool> AtualizarItem(Guid clienteId, Guid produtoId, int quantidade)
         {
             var pedido = await _pedidoRepository.ObterPedidoRascunhoPorClienteId(clienteId);
             if (pedido is null)
                 throw new DomainException("Pedido não encontrado!");
-
             var pedidoItem = await _pedidoRepository.ObterItemPorPedido(pedido.Id, produtoId);
             if (!pedido.PedidoItemExistente(pedidoItem))
                 throw new DomainException("Item do pedido não encontrado!");
-
             pedido.AtualizarUnidades(pedidoItem, quantidade);
             pedido.AdicionarEvento(new PedidoProdutoAtualizadoEvent(clienteId, pedido.Id, produtoId, quantidade));
-
             _pedidoRepository.AtualizarItem(pedidoItem);
             _pedidoRepository.Atualizar(pedido);
-
             return await _pedidoRepository.UnitOfWork.Commit();
         }
 
@@ -169,24 +175,6 @@ namespace Application.Pedidos.UseCases
             return await _pedidoRepository.UnitOfWork.Commit();
         }
 
-        #endregion
-
-        #region Metodos privados
-        private async Task<Pedido> BuscarOuIniciarPedidoAsync(Guid clienteId, Guid produtoId, PedidoItem pedidoItem)
-        {
-            var pedido = await _pedidoRepository.ObterPedidoRascunhoPorClienteId(clienteId);
-
-            if (pedido is not null)
-               return pedido;
-
-            pedido = Pedido.PedidoFactory.NovoPedidoRascunho(clienteId);
-            pedido.AdicionarItem(pedidoItem);
-
-            _pedidoRepository.Adicionar(pedido);
-            pedido.AdicionarEvento(new PedidoRascunhoIniciadoEvent(clienteId, produtoId));
-
-            return pedido;
-        }
         #endregion
 
         public void Dispose()
