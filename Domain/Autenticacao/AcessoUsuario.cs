843cea2405c6f5e290f84d92f81367c2fd711bac
Mateus Bernardi Marcato <mateus02_bernardi@hotmail.com>
2023-08-27 17:15:15 -0300

    Migrar mapeamento do Entity Framework para FluentAPI

--- Domain/Autenticacao/AcessoUsuario.cs
+++ Domain/Autenticacao/AcessoUsuario.cs
@@ -1,14 +1,12 @@
 ﻿using Domain.Autenticacao.Enums;
 using Domain.Base.DomainObjects;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Domain.Autenticacao
 {
-    [Table("acesso_usuario")]
     public class AcessoUsuario : Entity, IAggregateRoot
     {
         #region construtores
-        public AcessoUsuario() 
+        public AcessoUsuario()
         {
             NomeUsuario = string.Empty;
             Senha = string.Empty;
@@ -32,13 +30,10 @@ namespace Domain.Autenticacao
         }
         #endregion
 
-        [Column("nome_usuario")]
         public string NomeUsuario { get; private set; }
-        
-        [Column("senha")]
+
         public string Senha { get; private set; }
 
-        [Column("role_usuario")]
         public Roles Role { get; private set; }
 
         public void Validar()
