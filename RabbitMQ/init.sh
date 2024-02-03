5fd32ff33d1720dc2a0ec14d2bc2cd1db161ae38
Christian <christiandmelo@gmail.com>
2024-02-03 18:09:37 -0300

    Ajuste para subir um container do RabbiMQ

---
+++ RabbitMQ/init.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# init.sh
+
+# Função para esperar o RabbitMQ estar pronto
+wait_for_rabbitmq() {
+    echo "Aguardando RabbitMQ iniciar..."
+    while ! nc -z localhost 15672; do   
+      sleep 1 # Aguarda 1 segundo antes de verificar novamente
+    done
+    echo "RabbitMQ iniciado"
+}
+
+# Função para criar filas usando rabbitmqadmin
+create_queues() {
+    echo "Criando filas..."
+
+    # Exemplo de criação de fila
+    rabbitmqadmin declare queue name=fila_de_teste durable=true
+
+    # Exemplo de criação de exchange
+    #rabbitmqadmin declare exchange name=my_exchange type=direct
+
+    # Exemplo de criação de binding
+    #rabbitmqadmin declare binding source=my_exchange destination=my_queue routing_key=my_routing_key
+
+    echo "Filas criadas"
+}
+
+# Aguarda o RabbitMQ estar pronto
+wait_for_rabbitmq
+
+# Cria as filas
+create_queues
